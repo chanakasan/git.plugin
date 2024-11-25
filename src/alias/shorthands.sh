9030aa80028d6b16f5842a6cac8f142604b4bb9e
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-11-25 11:48:49 +0530

    refac

---
+++ src/alias/shorthands.sh
@@ -0,0 +1,12 @@
+# shorthands
+# alias gs="git status"
+# alias gco="git checkout"
+# alias gd="git diff"
+# alias gdc="git diff --cached"
+# alias gc="git commit"
+# alias ga="git add"
+# alias gr="g reset"
+
+# shorthands, doesn't take args
+alias glga="git log --oneline --graph --decorate --all --first-parent"
+alias glg="git log --oneline --graph --decorate --first-parent"
