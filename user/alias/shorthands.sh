4be99d4c245495d8b815c388e15d4cfe2973adf9
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-01-07 11:24:36 +0530

    rev

--- user/alias/shorthands.sh
+++ user/alias/shorthands.sh
@@ -2,7 +2,7 @@
 alias glga="git log --oneline --graph --decorate --all --first-parent"
 alias glg="git log --oneline --graph --decorate --first-parent"
 
-alias gc="git commit"
+alias gcom="git commit"
 alias gco="git checkout"
 alias ga="git add"
 alias gr="git reset"
