4dfd7535374416dbafc102b6dd35209c4d32e345
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-01-07 11:18:49 +0530

    rev

--- user/alias/shorthands.sh
+++ user/alias/shorthands.sh
@@ -1,3 +1,8 @@
 # shorthands, doesn't take args
 alias glga="git log --oneline --graph --decorate --all --first-parent"
 alias glg="git log --oneline --graph --decorate --first-parent"
+
+alias gcom="git commit"
+alias gc="git commit"
+alias ga="git add"
+alias gr="git reset"
